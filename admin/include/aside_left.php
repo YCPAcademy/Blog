5909b59c8f02fbff91babc669ef83890b8924537
bgarcia@ycomieronperdices.net <Andalucia1978>
2015-01-12 20:20:47 +0100

    start comiint

---
+++ admin/include/aside_left.php
@@ -0,0 +1,27 @@
+ <div class="navbar-default sidebar" role="navigation">
+            	
+    <div class="sidebar-nav navbar-collapse">
+    	
+        <ul class="nav" id="side-menu">
+        	
+            <li>
+                <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Home</a>
+            </li>
+            
+            <li>
+                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Blog<span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="<?php echo $base_url. "category.php" ?>">Categorías</a>
+                    </li>
+                    <li>
+                        <a href="">Publicaciones</a>
+                    </li>
+                </ul>
+                <!-- /.nav-second-level -->
+            </li> 
+        </ul>
+    </div>
+    <!-- /.sidebar-collapse -->
+</div>
+<!-- /.navbar-static-side -->
